b97f0f45be78a71930a702bfdd35b87917924c32
Sulkysubject37 <arshad10867c@gmail.com>
2026-08-09 15:13:05 +0530

    upG

--- interoperability/python/setup.py
+++ interoperability/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="biozig",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
-    description="Python wrapper for the BioZig C-ABI",
+    description="Python bindings for the BioZig computational biology engine",
 )
